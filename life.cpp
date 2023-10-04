fec42c3f609f5a48dfd4501ddc018ecd442d7015
Maxim Konoplia <49166705+thelimar@users.noreply.github.com>
2023-10-04 12:41:53 +0300

    Uploaded files

--- life.cpp
+++ life.cpp
@@ -1,40 +1,103 @@
 #include "life.h"
 #include <stdlib.h>
 #include <SFML/Graphics.hpp>
+#include <cstring> 
 
 
-void DrawGen(unsigned* gen)
-{
+void DrawGen(uint8_t* gen, sf::RenderWindow& window) {
+    window.clear();
+    sf::RectangleShape cell(sf::Vector2f(1, 1));
 
+    for (int y = 0; y < SIZE_Y; y++) 
+    {
+        for (int x = 0; x < SIZE_X; x++) 
+        {
+            if (gen[y * SIZE_X + x] == 1) 
+            {
+                cell.setPosition(x, y);
+                window.draw(cell);
+            }
+        }
+    }
+    window.display();
 }
 
-void CalcNextGen(unsigned* current_gen, unsigned* next_gen)
+void CalcNextGen(uint8_t* current_gen, uint8_t* next_gen)
 {
+    for (int y = 0; y < SIZE_Y; y++) 
+    {
+        for (int x = 0; x < SIZE_X; x++) 
+        {
+            int neighbors = 0;
+            for (int i = -1; i <= 1; i++) 
+            {
+                for (int j = -1; j <= 1; j++) 
+                {
+                    if (i == 0 && j == 0) continue;
+                    int nx = x + i;
+                    int ny = y + j;
 
+                    if (nx >= 0 && nx < SIZE_X && ny >= 0 && ny < SIZE_Y) 
+                    {
+                        neighbors += current_gen[ny * SIZE_X + nx];
+                    }
+                }
+            }
+
+
+            if (current_gen[y * SIZE_X + x] == 1) 
+            {
+                if (neighbors < 2 || neighbors > 3) 
+                {
+                    next_gen[y * SIZE_X + x] = 0;
+                } else 
+                {
+                    next_gen[y * SIZE_X + x] = 1;
+                }
+            } else 
+            {
+                if (neighbors == 3) 
+                {
+                    next_gen[y * SIZE_X + x] = 1;
+                } else 
+                {
+                    next_gen[y * SIZE_X + x] = 0;
+                }
+            }
+        }
+    }
 }
 
 
 int main()
 {
-    unsigned current_gen[SIZE_X * SIZE_Y];
-    unsigned next_gen[SIZE_X * SIZE_Y];
+    sf::RenderWindow window(sf::VideoMode(SIZE_X, SIZE_Y), "Game of Life");
 
+    uint8_t current_gen[SIZE_X * SIZE_Y];
+    uint8_t next_gen[SIZE_X * SIZE_Y];
 
-    for (unsigned y = 0; y < SIZE_Y; y++)
-        for (unsigned x = 0; x < SIZE_X; x++)
+    for (int y = 0; y < SIZE_Y; y++) 
+    {
+        for (int x = 0; x < SIZE_X; x++) {
+            current_gen[y * SIZE_X + x] = std::rand() % 2;
+        }
+    }
+
+    while (window.isOpen()) 
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) 
         {
-            current_gen[y * SIZE_X + x] = rand() % 2;
+            if (event.type == sf::Event::Closed) 
+            {
+                window.close();
+            }
         }
 
-    DrawGen(current_gen);
+        CalcNextGen(current_gen, next_gen);
+        DrawGen(next_gen, window);
 
-    for (unsigned i = 0; i < TOTAL_TICS; i++)
-    {
-        ClacNextGen(current_gen, next_gen);
-        DrawGen(next_gen);
-        unsigned *tmp = current_gen;
-        current_gen = next_gen;
-        next_gen = tmp;
+        std::swap(current_gen, next_gen);
     }
 
     return 0;
--- life.h
+++ life.h
@@ -1,3 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
 #define SIZE_X 600
 #define SIZE_Y 400
-#define TOTAL_TICS 3000
+
+void CalcNextGen(uint8_t* current_gen, uint8_t* next_gen);
+void DrawGen(uint8_t* gen, sf::RenderWindow& window);
